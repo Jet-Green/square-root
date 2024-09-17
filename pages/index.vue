f9edcc057e48d3b9c7307691cf7428296dc85909
JetGreen <grishadzyin@gmail.com>
2024-09-17 10:35:58 +0500

    start project

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup></script>
 
 <template>
-  <v-container>
-    sdfsd
-  </v-container>
+  <div class="blue-gradient">
+    <v-container class="d-flex justify-center align-center">
+      <span class="main-title">
+        Калькулятор
+      </span>
+      <!-- <div class="black-bg">
+        dads
+      </div> -->
+    </v-container>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main-title {
+  font-weight: 600;
+  font-size: clamp(2.25rem, 1.0568rem + 3.4091vw, 3.75rem);
+  user-select: none;
+  margin-top: 10vh;
+}
+</style>
