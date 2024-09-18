ab6ec988fc65f1e708f202a0c01006afbfdd63ff
JetGreen <grishadzyin@gmail.com>
2024-09-18 16:23:29 +0500

    add night sky and main gradient

---
+++ components/StarsAndMountains.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+onMounted(() => {
+  let stars: any = document.getElementById("stars")
+  let moon: any = document.getElementById("moon")
+  let mountains_behind: any = document.getElementById("mountains_behind")
+  let mountains_front: any = document.getElementById("mountains_front")
+  let text: any = document.getElementById("text")
+  let button: any = document.getElementById("button")
+
+  function parallax() {
+    let value = window.scrollY
+    stars.style.left = value * 0.25 + "px"
+    moon.style.top = value * 1.05 + "px"
+    mountains_behind.style.top = value * 0.5 + "px"
+    mountains_front.style.top = value * 0 + "px"
+    text.style.marginRight = value * 4 + "px"
+    text.style.marginTop = value * 1.5 + "px"
+    button.style.marginTop = value * 1.5 + "px"
+  }
+
+  window.addEventListener("scroll", parallax)
+})
+</script>
+<template>
+  <section style="
+    background: rgb(2,31,75);
+    min-height: 100vh;
+    /* background: linear-gradient(180deg, rgba(2,31,75,1) 0%, rgba(117,151,222,1) 75%); */
+  ">
+    <img src="../assets/images/stars.png" alt="stars" id="stars" />
+    <!-- <img src="../assets/images/moon.png" alt="moon" id="moon" /> -->
+    <img src="../assets/images/mountains_behind.png" alt="mountains_behind" id="mountains_behind" />
+    <h2 id="text">Moon Light</h2>
+    <span></span>
+    <img src="../assets/images/mountains_front.png" alt="mountains_front" id="mountains_front" />
+  </section>
+</template>
+<style scoped lang="scss">
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+section {
+  position: relative;
+  width: 100%;
+  height: 125vh;
+  padding: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+section::before {
+  /*blending the border between sections*/
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  background: linear-gradient(to top, #1c0522, transparent);
+  z-index: 1000;
+}
+
+section img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+
+section img#moon {
+  mix-blend-mode: screen;
+}
+
+section img#mountains_front {
+  z-index: 10;
+}
+
+#text {
+  right: -350px;
+  position: absolute;
+  color: #fff;
+  white-space: nowrap;
+  font-size: 7.5vw;
+  z-index: 9;
+}
+
+#button {
+  text-decoration: none;
+  padding: 8px 30px;
+  border-radius: 40px;
+  background-color: #fff;
+  color: #2b1055;
+  font-size: 1.5em;
+  display: inline-block;
+  z-index: 9;
+  transform: translateY(120px);
+}
+
+.sec {
+  position: relative;
+  padding: 100px;
+  background-color: #1c0522;
+}
+
+.sec h2 {
+  font-size: 3.5em;
+  margin-bottom: 10px;
+  color: #fff;
+}
+
+.sec p {
+  font-size: 1em;
+  color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,46 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="blue-gradient">
+  <div class="descent-gradient">
     <v-container class="d-flex justify-center align-center">
-      <span class="main-title">
-        Калькулятор
-      </span>
+      <span class="main-title"> Calculator </span>
       <!-- <div class="black-bg">
         dads
       </div> -->
     </v-container>
   </div>
+  <ClientOnly>
+    <StarsAndMountains />
+  </ClientOnly>
 </template>
 
 <style scoped lang="scss">
 .main-title {
-  font-weight: 600;
-  font-size: clamp(2.25rem, 1.0568rem + 3.4091vw, 3.75rem);
+  font-weight: 800;
+  font-size: clamp(3.4375rem, 2.4537rem + 5.2469vw, 8.75rem);
   user-select: none;
-  margin-top: 10vh;
+  margin-top: 20vh;
+
+  // цвет текста
+  background: rgb(255, 159, 64);
+  background: linear-gradient(180deg, rgba(255, 159, 64, 0.9) 50%, rgba(60, 206, 241, 0.3) 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  // цвет текста
+}
+
+.descent-gradient {
+  background: rgb(108, 215, 241);
+  background: linear-gradient(
+    180deg,
+    rgba(108, 215, 241, 1) 0%,
+    rgba(60, 206, 241, 1) 25%,
+    rgba(0, 183, 226, 1) 50%,
+    rgba(42, 145, 169, 1) 75%,
+    rgba(2, 31, 75, 1) 100%
+  );
+
+  height: 800vh;
 }
 </style>
